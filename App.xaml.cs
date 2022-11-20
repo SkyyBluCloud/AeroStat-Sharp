5cbd7130feab7e657119acabb440195a7fe0cd38
Shane DeSouza <shane.desouza.1@us.af.mil>
2022-11-19 17:46:07 -0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using AeroStat_Sharp.Modules;
+using AeroStat_Sharp.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System;
+using System.Windows;
+
+namespace AeroStat_Sharp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<Main>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<TrafficLogView>();
+        }
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            base.ConfigureModuleCatalog(moduleCatalog);
+            moduleCatalog.AddModule<MainModule>();
+        }
+    }
+}
---
+++ Modules/MainModule.cs
@@ -0,0 +1,26 @@
+﻿using AeroStat_Sharp.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AeroStat_Sharp.Modules
+{
+    class MainModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            var region = containerProvider.Resolve<IRegionManager>();
+            region.RegisterViewWithRegion("MainRegion", typeof(TabView));
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+}
